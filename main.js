27e3be785316fc361604f28406a2aa8c3cfd06b3
Guilherme G <98757828+GuilhermeGavioli@users.noreply.github.com>
2022-06-14 22:14:03 -0300

    implementing js functionalty

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const addButton = document.getElementById("add-task-btn")
 const removeButton = document.getElementById("remove-task-btn") 
 const updateButton = document.getElementById("update-task-btn")
 
-const allTasksContainer = document.getElementById("tasks-container")
+const allTasksContainer = document.getElementById("tasks-container-list")
 
 
 addButton.addEventListener('click', async () => { 
@@ -16,27 +16,27 @@ addButton.addEventListener('click', async () => {
     const validation = validateTask(inputValue)
 
     if (validation.status) {
-        allTasksContainer.append(validation.task)
-        Input.clearInput()
 
-
-        return 
+        append(validation.task);
+        Input.clearInput();
+        return
     }
 
     alert("Invalid task: " + validation.message)
     
 
-
 })
 
-removeButton.addEventListener('click', (taskID) => { 
-    //add task
+function removeTask(taskID) { 
+    console.log(taskID)
+    //remove task
 
-})
-updateButton.addEventListener('click', (taskID) => { 
-    //add task
+}
 
-})
+function updateTask(taskID) { 
+
+
+}
 
 function validateTask(task) {
     if (!task) return { status: false, message: "Empty field or something else" };
@@ -46,6 +46,28 @@ function validateTask(task) {
 class Input {
     static getInputValue() { return addTaskInput.value }
     static clearInput() { return addTaskInput.value = "" }
+}
+
+
+function append(task) { 
+    const createdLi = document.createElement("li");
+    createdLi.setAttribute("id", Math.random().toString())
+
+    const createdDeleteButton = document.createElement("button");
+    createdDeleteButton.className = "w-10 h-10 bg-green-400"
+    createdDeleteButton.innerText = "delete"
+
+    const createdUpdateButton = document.createElement("button");
+    createdUpdateButton.innerText = "update"
+
+    createdLi.append(task);
+    createdLi.append(createdDeleteButton);
+    createdLi.append(createdUpdateButton);
+    allTasksContainer.append(createdLi);
+
+
+    createdDeleteButton.addEventListener("click", removeTask(createdLi.getAttribute("id")));
+    createdUpdateButton.addEventListener("click", updateTask(createdLi.getAttribute("id")));
 }
 
 
